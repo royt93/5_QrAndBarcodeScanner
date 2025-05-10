b3d67394ec1a1d4859a73e66c03d884b6e8a573b
roy <loitp@galaxyjoy.vn>
2025-05-10 10:46:10 +0700

    update

--- app/src/main/java/com/mckimquyen/barcodescanner/feature/SplashActivity.kt
+++ app/src/main/java/com/mckimquyen/barcodescanner/feature/SplashActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.startActivity
 import com.mckimquyen.barcodescanner.BuildConfig
 import com.mckimquyen.barcodescanner.databinding.ActivitySplashBinding
 import com.mckimquyen.barcodescanner.feature.tabs.ActivityBottomTabs
@@ -29,7 +30,10 @@ class SplashActivity : AppCompatActivity() {
     private fun goToMain() {
         val intent = Intent(this@SplashActivity, ActivityBottomTabs::class.java)
         startActivity(intent)
-        overridePendingTransition(0, 0)
-        finishAffinity()
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+        // Trì hoãn finish để đợi animation hoàn tất
+        window.decorView.postDelayed({
+            finish() // Finish sau animation
+        }, 300) // delay khoảng 300ms (hoặc đúng thời gian của animation)
     }
 }
